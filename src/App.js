2c9d47cb020c8a99667b9e056e3d15788386d7ba
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-02 19:21:01 -0300

    feat: add initial page routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import Wallet from "./pages/Wallet";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import UserContextProvider from "./contexts/UserContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-export default function App(){
-    return(
-        <h1>aqui</h1>
+export default function App() {
+    return (
+        <BrowserRouter>
+            <UserContextProvider>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/wallet" element={<Wallet />} />
+                </Routes>
+            </UserContextProvider>
+        </BrowserRouter>
     );
 }
